ee0cb4d6bced4592414ff6b92dbf78b0356f6172
lmvicente <leah.m.vicente@gmail.com>
2018-12-20 15:58:00 -0800

    lab7 finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ToastService } from './toast/toast.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toast-app';
+  toastTypes: Array<string> = [];
+
+  constructor(private toastService: ToastService) {
+    this.toastTypes = ['success', 'info', 'warning', 'danger'];
+  }
+
+  // success, info, warning, danger
+  showToast() {
+    const rand = Math.floor(Math.random() * 4);
+    // console.log('Random Number: ' + rand);
+    const toastType = this.toastTypes[rand];
+    const toastMessage = 'The random number is .. ' + rand + '!';
+    const duration = 3000;
+    this.toastService.showToast(toastType, toastMessage, duration);
+  }
 }
